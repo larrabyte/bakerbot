68ee02cabb8e585345b4f09360304d707c9eb52f
larrabyte <larrabyte@protonmail.ch>
2019-09-14 17:36:37 +1000

    Add help cog
    Added getembed() in the utilities module as a helper function.

---
+++ cogs/help.py
@@ -0,0 +1,29 @@
+"""A cog implementing the `help` function."""
+from discord.ext import commands
+import utilities as util
+import importlib
+import discord
+
+def loadcog(bot, cogname):
+    modified = cogname.replace(".py", "").replace("cogs.", "")
+    return bot.get_cog(modified)
+
+class help(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["helpme"])
+    async def help(self, ctx, cogname: str=None):
+        """Help function. Optionally, takes in `cogname`."""
+        embed = util.getembed("Bakerbot: Help menu.", 0xFF8C00)
+        modules = [(importlib.import_module(util.getcogname(cogs)), util.getcogname(cogs)) for cogs in util.fetchcogs()]
+
+        if cogname:
+            cog = loadcog(self.bot, cogname)
+            for command in cog.get_commands(): embed.add_field(name=str(command), value=command.help, inline=False)
+        else: 
+            for mods in modules: embed.add_field(name=mods[1], value=mods[0].__doc__, inline=False)
+
+        await ctx.send(embed=embed)
+
+def setup(bot): bot.add_cog(help(bot))
--- utilities.py
+++ utilities.py
@@ -11,3 +11,12 @@ def getcogname(filename):
     Example: `errorhandler.py` >> `cogs.errorhandler`"""
     filename = filename.replace(".py", "")
     return "cogs." + filename
+
+def getembed(title, colour, footerText: str=None):
+    """Returns a discord Embed with `title`, `colour` and optionally, `footerText`.\n
+       Also adds a picture of someone from Airbus called Anthony Baker."""
+    embed = discord.Embed(title=title, color=colour)
+    embed.set_thumbnail(url="https://airbus-h.assetsadobe2.com/is/image/content/dam/channel-specific/website-/us/management/anthony-baker.jpg?wid=1000&qlt=85,0")
+    if footerText: embed.set_footer(text=footerText)
+    else: embed.set_footer(text="creeper, aww man")
+    return embed
