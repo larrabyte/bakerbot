d0b3410d92f4085c3dfcd361e50825237f38922c
larrabyte <larrabyte@protonmail.ch>
2020-12-24 23:30:15 +1100

    Make the on_command_error() output not so ugly

--- cogs/debugger.py
+++ cogs/debugger.py
@@ -8,16 +8,24 @@ class debugger(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-        """Throws exceptions to Discord."""
+        """Throws any uncaught exceptions to Discord."""
         if hasattr(ctx.command, "on_error"): return
-        embed = getembed("Bakerbot: Unhandled exception!", f"Raised by {ctx.author.mention} while using ${ctx.command}.", 0xFF0000)
-        embed.add_field(name=f"{error}.", value="Try again with different arguments or contact the bot author for help.", inline=False)
+        footer = f"Raised by {ctx.author.name} while using ${ctx.command}." if ctx.command else f"Raised by {ctx.author.name} while trying to use a non-existent command."
+        errstr = str(error) if str(error)[-1] == "." else f"{error}."
+
+        embed = getembed("Bakerbot: Unhandled exception!", footer, 0xFF0000)
+        embed.add_field(name=errstr, value="Try again with different arguments or contact the bot author for help.", inline=False)
         await ctx.send(embed=embed)
 
     @commands.is_owner()
     @commands.command()
-    async def loadjsk(self, ctx):
-        """Jishaku bootstrapper. Only the owner may execute this command."""
-        self.bot.load_extension("jishaku")
+    async def jskswitch(self, ctx):
+        """Bootstraps or unloads Jishaku through a simple self.bot() call."""
+        if self.bot.cogs["jishaku"]:
+            self.bot.unload_extension("jishaku")
+            await ctx.send("Jishaku debugger disabled.")
+        else:
+            self.bot.load_extension("jishaku")
+            await ctx.send("Jishaku debugger enabled.")
 
 def setup(bot): bot.add_cog(debugger(bot))
