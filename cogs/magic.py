caa9737a98f92c076d12a67b4f4c04653d4564a5
larrabyte <larrabyte@protonmail.ch>
2021-05-23 13:41:37 +1000

    Add webhook multiplexing
    So it turns out that the webhook limit per channel is only enforced by Discord clients (and *not* by the API).

--- cogs/magic.py
+++ cogs/magic.py
@@ -1,20 +1,23 @@
 from libs.utilities import Embeds, Colours, Icons
 from libs.models import Bakerbot
 from discord.ext import commands
+import secrets
 
+import aiohttp
+import asyncio
 import discord
 import random
 
 class Magic(commands.Cog):
     """You can find dumb ideas from Team Magic here."""
     def __init__(self, bot: Bakerbot):
-        self.magic_guild_id = 473426067823263749
+        self.guild_id = 473426067823263749
         self.omd_enable = False
         self.bot = bot
 
     async def cog_check(self, ctx: commands.Context) -> None:
         # Ensure that these commands can only be run from Team Magic.
-        if ctx.guild.id == self.magic_guild_id:
+        if ctx.guild.id == self.guild_id:
             return True
 
         return False
@@ -53,12 +56,43 @@ class Magic(commands.Cog):
         embed.set_footer(text="Enjoy your fetish.", icon_url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
+    @magic.command()
+    async def demux(self, ctx: commands.Context, channel: discord.TextChannel, *, message: str) -> None:
+        """A demultiplexer experiment using Discord webhooks."""
+        endpoints = await channel.webhooks()
+        user, avatar = ctx.author.display_name, self.bot.user.avatar_url
+        tasks = [hook.send(message, username=user, avatar_url=avatar) for hook in endpoints]
+        await asyncio.gather(*tasks)
+
+    @magic.command()
+    async def hookify(self, ctx: commands.Context, target: discord.TextChannel) -> None:
+        """So it turns out you can have more than 10 webooks in a channel..."""
+        headers = { "Authorization": f"Bot {secrets.dpytoken}" }
+        payload = { "channel_id": target.id }
+
+        endpoints = await ctx.channel.webhooks()
+        moved = 0
+
+        for hook in endpoints:
+            url = f"https://discord.com/api/webhooks/{hook.id}"
+            resp = await self.bot.session.patch(url, json=payload, headers=headers)
+            moved += 1 if resp.status == 200 else 0
+
+            if resp.status != 200:
+                await ctx.send(f"HTTP {resp.status} {resp.reason}\n"
+                               f"Webhook {hook.id} was not moved.\n\n"
+                               f"{resp.headers}")
+
+                break
+
+        await ctx.send(f"{moved} webhooks were moved.")
+
     @commands.Cog.listener()
     async def on_message_delete(self, message: discord.Message) -> None:
         # Resends deleted messages. Triggered by the nodelete command.
         if not self.omd_enable: return
 
-        if message.guild.id == self.magic_guild_id:
+        if message.guild.id == self.guild_id:
             await message.channel.send(f"> Sent by {message.author.mention}\n{message.content}")
 
 def setup(bot): bot.add_cog(Magic(bot))
