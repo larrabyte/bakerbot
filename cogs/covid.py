01b7b933b8ef00a761e145c91a493c0952b9bb05
larrabyte <larrabyte@protonmail.ch>
2021-08-22 13:44:44 +1000

    Add a Statistics object for the COVID API

--- cogs/covid.py
+++ cogs/covid.py
@@ -6,7 +6,6 @@ import datetime
 import discord
 import asyncio
 import model
-import re
 
 class Covid(commands.Cog):
     """Daily COVID-19 tracker for New South Wales."""
@@ -16,29 +15,33 @@ class Covid(commands.Cog):
 
         # Start the statistics task if this channel exists in the bot's list.
         if (channel := bot.get_channel(473426067823263753)) is not None:
-            self.covid_task.start(channel)
+            self.task.start(channel)
 
     def cog_unload(self):
         """Handles task cancellation on cog unload."""
-        self.covid_task.cancel()
+        self.task.cancel()
 
-    def covid_embed(self, results: dict) -> discord.Embed:
+    def embeddify(self, results: covid.Statistics) -> discord.Embed:
         """Creates and returns a COVID-19 statistics embed."""
         time = datetime.datetime.utcnow().strftime("%A, %d %B %Y")
+        embed = utilities.Embeds.standard(title=f"COVID-19 Statistics as of {time}")
+        embed.set_footer(text="Data taken from the NSW Data Analytics Centre.", icon_url=utilities.Icons.info)
 
-        embed = utilities.Embeds.standard()
-        footer = "Data taken from the NSW Data Analytics Centre."
-        embed.set_footer(text=footer, icon_url=utilities.Icons.info)
-        embed.title = f"COVID-19 Statistics as of {time}:"
+        acquisitions = f"With a total of **{results.new:,}** cases:\n"
+        acquisitions += f"* **{results.local:,}** cases were acquired locally.\n"
+        acquisitions += f"* **{results.interstate:,}** cases were acquired interstate.\n"
+        acquisitions += f"* **{results.overseas:,}** cases were acquired overseas."
+        embed.add_field(name="New Acquisitions", value=acquisitions)
 
-        for key, value in results["data"][0].items():
-            words = re.findall("[A-Z][^A-Z]*", key)
-            embed.add_field(name=" ".join(words), value=f"{value:,}")
-
+        totals = f"* **{results.tested:,}** tests have been administered.\n"
+        totals += f"* **{results.cases:,}** cases have been recorded.\n"
+        totals += f"* **{results.recovered:,}** people have recovered.\n"
+        totals += f"* **{results.deaths:,}** deaths have been recorded."
+        embed.add_field(name="Current State Totals", value=totals)
         return embed
 
     @tasks.loop(hours=24)
-    async def covid_task(self, channel: discord.TextChannel) -> None:
+    async def task(self, channel: discord.TextChannel) -> None:
         """Asynchronous task for displaying statistics every 24 hours."""
         now = datetime.datetime.now()
         execat = datetime.time(20, 30)
@@ -52,15 +55,15 @@ class Covid(commands.Cog):
         seconds = (delay - now).total_seconds()
         await asyncio.sleep(seconds)
 
-        results = await self.backend.request("datafiles/statsLocations.json")
-        embed = self.covid_embed(results)
+        results = await self.backend.statistics()
+        embed = self.embeddify(results)
         await channel.send(embed=embed)
 
     @commands.command()
     async def covid(self, ctx: commands.Context) -> None:
         """Query the API for COVID-19 statistics in New South Wales."""
-        results = await self.backend.request("datafiles/statsLocations.json")
-        embed = self.covid_embed(results)
+        results = await self.backend.statistics()
+        embed = self.embeddify(results)
         await ctx.reply(embed=embed)
 
 def setup(bot: model.Bakerbot) -> None:
--- libs/covid.py
+++ libs/covid.py
@@ -2,6 +2,18 @@ import aiohttp
 import ujson
 import yarl
 
+class Statistics:
+    """A COVID-19 data object."""
+    def __init__(self, data: dict) -> None:
+        self.recovered = data.get("Recovered", None)
+        self.overseas = data.get("AcquiredOverseas", None)
+        self.local = data.get("AcquiredLocally", None)
+        self.deaths = data.get("Deaths", None)
+        self.new = data.get("NewCases", None)
+        self.tested = data.get("Tested", None)
+        self.cases = data.get("Cases", None)
+        self.interstate = data.get("AcquiredInterstate", None)
+
 class Backend:
     """Backend COVID-19 API wrapper."""
     def __init__(self, session: aiohttp.ClientSession) -> None:
@@ -16,3 +28,10 @@ class Backend:
             data = await resp.read()
             data = data.decode("utf-8")
             return ujson.loads(data)
+
+    async def statistics(self) -> Statistics:
+        """Returns COVID-19 statistics from the NSWDAC's API."""
+        results = await self.request("datafiles/statsLocations.json")
+        results = results["data"][0]
+        stats = Statistics(results)
+        return stats
