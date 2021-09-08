40a424d80dfb36bf0e4ebdf19e238e5834ef040f
larrabyte <larrabyte@protonmail.ch>
2021-09-08 10:29:46 +1000

    Improve the help command interface

--- cogs/helper.py
+++ cogs/helper.py
@@ -12,11 +12,9 @@ class Helper(commands.Cog):
     @commands.command()
     async def help(self, ctx: commands.Context) -> None:
         """The help command for Bakerbot."""
-        embed = utilities.Embeds.standard()
-        embed.description = "Use the dropbown menu below to see the help for a specific command group."
-        embed.set_footer(text="Remember: the prefix is $ (and not +).", icon_url=utilities.Icons.info)
+        instructions = "Use the dropbown menu below to see help for a specific command group."
         view = HelperView(self.bot)
-        await ctx.reply(embed=embed, view=view)
+        await ctx.reply(instructions, view=view)
 
 class HelperView(discord.ui.View):
     """A subclass of `discord.ui.View` for the help screen."""
@@ -28,7 +26,7 @@ class HelperView(discord.ui.View):
         self.menu.callback = self.cog_callback
 
         for name, cog in self.bot.cogs.items():
-            self.menu.add_option(label=name.lower(), value=name, description=cog.description)
+            self.menu.add_option(label=cog.__class__.__module__, value=name, description=cog.description)
 
         self.add_item(self.menu)
 
@@ -37,7 +35,8 @@ class HelperView(discord.ui.View):
         selection = self.menu.values[0]
         cog = self.bot.cogs[selection]
 
-        embed = utilities.Embeds.standard()
+        embed = utilities.Embeds.standard(description=cog.description)
+        embed.title = f"Documentation for `{cog.__class__.__module__}`:"
         footer = "Arguments enclosed in <> are required while [] are optional."
         embed.set_footer(text=footer, icon_url=utilities.Icons.info)
 
@@ -47,9 +46,10 @@ class HelperView(discord.ui.View):
                 continue
 
             prefix = f"{command.full_parent_name} " if command.parent else ""
-            embed.add_field(name=f"{prefix}{command.name} {command.signature}", value=command.help, inline=False)
+            signature = f"${prefix}{command.name} {command.signature}"
+            embed.add_field(name=signature, value=command.help)
 
-        await interaction.response.edit_message(embed=embed)
+        await interaction.response.edit_message(content=None, embed=embed)
 
 def setup(bot: model.Bakerbot) -> None:
     cog = Helper(bot)
