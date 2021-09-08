7d875330b9170f6f2a3ea847f946d19764b1f88c
larrabyte <larrabyte@protonmail.ch>
2021-09-08 10:50:47 +1000

    Add support to help for specifying initial cog

--- cogs/helper.py
+++ cogs/helper.py
@@ -2,6 +2,7 @@ import backend.utilities as utilities
 import model
 
 import discord.ext.commands as commands
+import typing as t
 import discord
 
 class Helper(commands.Cog):
@@ -9,32 +10,8 @@ class Helper(commands.Cog):
     def __init__(self, bot: model.Bakerbot) -> None:
         self.bot = bot
 
-    @commands.command()
-    async def help(self, ctx: commands.Context) -> None:
-        """The help command for Bakerbot."""
-        instructions = "Use the dropbown menu below to see help for a specific command group."
-        view = HelperView(self.bot)
-        await ctx.reply(instructions, view=view)
-
-class HelperView(discord.ui.View):
-    """A subclass of `discord.ui.View` for the help screen."""
-    def __init__(self, bot: model.Bakerbot, *args: list, **kwargs: dict) -> None:
-        super().__init__(*args, **kwargs)
-        self.bot = bot
-
-        self.menu = discord.ui.Select(placeholder="Select any cog to view its commands.")
-        self.menu.callback = self.cog_callback
-
-        for name, cog in self.bot.cogs.items():
-            self.menu.add_option(label=cog.__class__.__module__, value=name, description=cog.description)
-
-        self.add_item(self.menu)
-
-    async def cog_callback(self, interaction: discord.Interaction) -> None:
-        """Handles cog help requests."""
-        selection = self.menu.values[0]
-        cog = self.bot.cogs[selection]
-
+    def embeddify(self, cog: commands.Cog) -> discord.Embed:
+        """Creates a help embed for `cog`."""
         embed = utilities.Embeds.standard(description=cog.description)
         embed.title = f"Documentation for `{cog.__class__.__module__}`:"
         footer = "Arguments enclosed in <> are required while [] are optional."
@@ -49,6 +26,42 @@ class HelperView(discord.ui.View):
             signature = f"${prefix}{command.name} {command.signature}"
             embed.add_field(name=signature, value=command.help)
 
+        return embed
+
+    @commands.command()
+    async def help(self, ctx: commands.Context, cog: t.Optional[str]) -> None:
+        """The help command for Bakerbot."""
+        view = HelperView(self.bot.cogs, self.embeddify)
+
+        if cog is None:
+            instructions = "Use the dropbown menu below to see help for a specific command group."
+            await ctx.reply(instructions, view=view)
+        else:
+            cog = self.bot.cogs[cog.capitalize()]
+            embed = self.embeddify(cog)
+            await ctx.reply(embed=embed, view=view)
+
+class HelperView(discord.ui.View):
+    """A subclass of `discord.ui.View` for the help screen."""
+    def __init__(self, cogs: t.Mapping[str, commands.Cog], formatter: t.Callable, *args: list, **kwargs: dict) -> None:
+        super().__init__(*args, **kwargs)
+        self.formatter = formatter
+        self.cogs = cogs
+
+        self.menu = discord.ui.Select(placeholder="Select any cog to view its commands.")
+        self.menu.callback = self.cog_callback
+
+        for name, cog in self.cogs.items():
+            label = cog.__class__.__module__
+            self.menu.add_option(label=label, value=name, description=cog.description)
+
+        self.add_item(self.menu)
+
+    async def cog_callback(self, interaction: discord.Interaction) -> None:
+        """Handles cog help requests."""
+        selection = self.menu.values[0]
+        cog = self.cogs[selection]
+        embed = self.formatter(cog)
         await interaction.response.edit_message(content=None, embed=embed)
 
 def setup(bot: model.Bakerbot) -> None:
