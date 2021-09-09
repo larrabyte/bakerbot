b44c7f25e7cefa50be3098da4e6325f5d5a2cf67
larrabyte <larrabyte@protonmail.ch>
2021-09-09 18:20:57 +1000

    Move Team Magic listeners into separate classes

--- cogs/magic.py
+++ cogs/magic.py
@@ -9,8 +9,8 @@ class Magic(commands.Cog):
     """You can find dumb ideas from Team Magic here."""
     def __init__(self, bot: model.Bakerbot):
         self.magic = 473426067823263749
-        self.targets = set()
-        self.sniper = False
+        self.sniper = MessageResender(bot, self.magic)
+        self.disconnecter = Disconnecter(bot, self.magic)
         self.bot = bot
 
     async def cog_check(self, ctx: commands.Context) -> None:
@@ -26,11 +26,9 @@ class Magic(commands.Cog):
 
     @magic.command()
     async def nodelete(self, ctx: commands.Context) -> None:
-        """Enables/disables the `on_message` listener for Team Magic."""
-        self.sniper = not self.sniper
-        description = f"on_message_delete() listener set to: `{self.sniper}`"
-        embed = utilities.Embeds.status(True, description)
-        await ctx.reply(embed=embed)
+        """Enables/disables the `on_message_delete()` listener for Team Magic."""
+        self.sniper.enabled = not self.sniper.enabled
+        await ctx.reply(f"`on_message_delete()` listener is (now?) set to: `{self.sniper.enabled}`")
 
     @magic.command()
     async def demux(self, ctx: commands.Context, channel: discord.TextChannel, *, message: str) -> None:
@@ -62,27 +60,53 @@ class Magic(commands.Cog):
         await ctx.reply(f"{moved} webhooks were moved.")
 
     @magic.command()
-    async def gryffindor(self, ctx: commands.Context, member: discord.Member) -> None:
+    async def gandalf(self, ctx: commands.Context, member: discord.Member) -> None:
         """Prevents a member from joining a voice channel."""
         mentions = discord.AllowedMentions.none()
 
-        if member.id in self.targets:
-            self.targets.remove(member.id)
+        if member.id in self.disconnecter.targets:
+            self.disconnecter.targets.remove(member.id)
             await ctx.reply(f"{member.mention} shall pass!", allowed_mentions=mentions)
         else:
-            self.targets.add(member.id)
+            self.disconnecter.targets.add(member.id)
             if member.voice is not None and member.voice.channel is not None:
                 await member.move_to(None)
 
             await ctx.reply(f"{member.mention} shall not pass!", allowed_mentions=mentions)
 
-    @commands.Cog.listener()
+class MessageResender:
+    """A wrapper around `on_message_delete()` for Team Magic."""
+    def __init__(self, bot: model.Bakerbot, guild: int) -> None:
+        self.ignore = set()
+        self.guild = guild
+        self.enabled = False
+
+        bot.loop.create_task(self.initialise(bot))
+        bot.add_listener(self.on_message_delete)
+
+    async def initialise(self, bot: model.Bakerbot) -> None:
+        """Initialises a `MessageResender` instance."""
+        if bot.user is None:
+            await bot.wait_until_ready()
+
+        self.ignore.add(bot.user.id)
+
     async def on_message_delete(self, message: discord.Message) -> None:
         """Team Magic-specific listener for resending deleted messages."""
-        if self.sniper and message.guild.id == self.magic:
-            await message.channel.send(f"> Sent by {message.author.mention}\n{message.content}")
+        if self.enabled and message.author.id not in self.ignore and message.guild.id == self.guild:
+            embed = discord.Embed(colour=utilities.Colours.REGULAR, timestamp=message.created_at)
+            embed.set_author(name=message.author.display_name, icon_url=message.author.display_avatar.url)
+            embed.set_footer(text="NUTS!", icon_url=utilities.Icons.INFO)
+            embed.description = message.content
+            await message.channel.send(embed=embed)
 
-    @commands.Cog.listener()
+class Disconnecter:
+    """A wrapper around `on_voice_state_update()` for Team Magic."""
+    def __init__(self, bot: model.Bakerbot, guild: int) -> None:
+        self.guild = guild
+        self.targets = set()
+        bot.add_listener(self.on_voice_state_update)
+
     async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState) -> None:
         """Team Magic-specific listener for preventing members from joining voice channels."""
         if member.id in self.targets and after.channel is not None:
