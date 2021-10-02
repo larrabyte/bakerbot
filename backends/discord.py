08c48b893b1469aacd310a5fa9863df786c545d8
larrabyte <larrabyte@protonmail.ch>
2021-10-02 14:07:46 +1000

    Add basic Go Live support
    No stream data actually goes through right now.

--- backends/discord.py
+++ backends/discord.py
@@ -1,8 +1,11 @@
 import exceptions
 import model
 
+import typing as t
 import discord
+import aiohttp
 import ujson
+import types
 import http
 
 class Backend:
@@ -10,7 +13,8 @@ class Backend:
     def setup(cls, bot: model.Bakerbot) -> None:
         cls.base = "https://discord.com/api/v9"
         cls.session = bot.session
-        cls.token: bot.secrets.get("discord-token", None)
+        cls.token = bot.secrets.get("discord-token", None)
+        cls.gateway_url = ""
 
     @classmethod
     async def get(cls, endpoint: str, **kwargs: dict) -> dict:
@@ -36,6 +40,15 @@ class Backend:
 
             return data
 
+    @classmethod
+    async def gateway(cls) -> str:
+        """Returns the result of the `/gateway` endpoint."""
+        if not cls.gateway_url:
+            result = await cls.get("gateway")
+            cls.gateway_url = result["url"]
+
+        return cls.gateway_url
+
 class Webhooks:
     @staticmethod
     async def create(channel: discord.TextChannel, name: str) -> None:
@@ -58,6 +71,122 @@ class Webhooks:
         payload = {"channel_id": channel.id}
         endpoint = f"webhooks/{webhook.id}"
         await Backend.post(endpoint, json=payload, headers=headers)
+
+class Gateway:
+    DISPATCH = 0
+    HEARTBEAT = 1
+    IDENTIFY = 2
+    PRESENCE_UPDATE = 3
+    VOICE_STATE_UPDATE = 4
+    RESUME = 6
+    RECONNECT = 7
+    REQUEST_QUILD_MEMBERS = 8
+    INVALID_SESION = 9
+    HELLO = 10
+    HEARTBEAT_ACK = 11
+    GUILD_SYNC = 12
+    START_STREAM = 18
+    DELETE_STREAM = 19
+
+    @staticmethod
+    async def wait_for(user: "User", predicate: t.Optional[t.Callable]=None) -> dict:
+        """Waits for the first event that satisfies the given predicate."""
+        while (response := await user.receive()):
+            if predicate is None or predicate(response) == True:
+                return response
+
+class User:
+    def __init__(self, token: str) -> None:
+        self.token = token
+
+    async def __aenter__(self) -> "User":
+        gateway = await Backend.gateway() + "/?v=9&encoding=json"
+        self.ws = await Backend.session.ws_connect(gateway)
+
+        hello = await self.receive()
+        self.heartbeat_interval = hello["d"]["heartbeat_interval"]
+
+        authentication = {
+            "op": Gateway.IDENTIFY,
+            "d": {
+                "token": self.token,
+                "compress": False,
+                "properties": {
+                    "os": "Linux",
+                    "browser": "Firefox",
+                    "device": ""
+                }
+            }
+        }
+
+        await self.send(authentication)
+
+        ready = await self.receive()
+        self.session_id = ready["d"]["session_id"]
+
+        return self
+
+    async def __aexit__(self, exception_type: BaseException, exception: Exception, traceback: types.TracebackType) -> None:
+        await self.ws.close()
+
+    async def send(self, data: dict) -> None:
+        """A wrapper around `send_json()` using ujson."""
+        return await self.ws.send_json(data, dumps=ujson.dumps)
+
+    async def receive(self) -> dict:
+        """A wrapper around `receive_json()` using ujson."""
+        return await self.ws.receive_json(loads=ujson.loads)
+
+    async def connect(self, channel: discord.VoiceChannel) -> None:
+        """Connects this user to a voice channel."""
+        payload = {
+            "op": Gateway.VOICE_STATE_UPDATE,
+            "d": {
+                "guild_id": channel.guild.id,
+                "channel_id": channel.id,
+                "self_mute": False,
+                "self_deaf": False,
+                "self_video": False
+            }
+        }
+
+        await self.send(payload)
+
+        audio_context = await Gateway.wait_for(self, lambda e: e["op"] == 0 and e["t"] == "VOICE_STATE_UPDATE")
+        audio_server = await Gateway.wait_for(self, lambda e: e["op"] == 0 and e["t"] == "VOICE_SERVER_UPDATE")
+
+    async def disconnect(self) -> None:
+        """Disconnects the user from any currently connected channels."""
+        payload = {
+            "op": Gateway.VOICE_STATE_UPDATE,
+            "d": {
+                "guild_id": None,
+                "channel_id": None,
+                "self_mute": False,
+                "self_deaf": False,
+                "self_video": False
+            }
+        }
+
+        await self.send(payload)
+
+    async def stream(self, channel: discord.VoiceChannel) -> None:
+        """Starts a Go Live stream in the user's current voice channel."""
+        payload = {
+            "op": Gateway.START_STREAM,
+            "d": {
+                "type": "guild",
+                "guild_id": channel.guild.id,
+                "channel_id": channel.id,
+                "preferred_region": None
+            }
+        }
+
+        await self.send(payload)
+
+        stream_authorisation = await Gateway.wait_for(self, lambda e: e["op"] == 0 and e["t"] == "STREAM_CREATE")
+        stream_context = await Gateway.wait_for(self, lambda e: e["op"] == 0 and e["t"] == "VOICE_STATE_UPDATE")
+        stream_server = await Gateway.wait_for(self, lambda e: e["op"] == 0 and e["t"] == "STREAM_SERVER_UPDATE")
 
 def setup(bot: model.Bakerbot) -> None:
     Backend.setup(bot)
--- cogs/random.py
+++ cogs/random.py
@@ -1,3 +1,5 @@
+import backends.discord as expcord
+import exceptions
 import utilities
 import model
 
@@ -6,6 +8,7 @@ import discord
 import asyncio
 import random
 import string
+import ujson
 import io
 
 class Random(commands.Cog):
@@ -13,19 +16,41 @@ class Random(commands.Cog):
     def __init__(self, bot: model.Bakerbot) -> None:
         self.bot = bot
 
-    @commands.group()
-    async def discord(self, ctx: commands.Context) -> None:
+    @commands.group(aliases=["discord"])
+    async def discordo(self, ctx: commands.Context) -> None:
         """The parent command for Discord experiments."""
         summary = ("You've encountered Bakerbot's Discord experimentation lab! "
                    "See `$help random` for a full list of available subcommands.")
 
         await utilities.Commands.group(ctx, summary)
 
-    @discord.command()
+    @discordo.command()
     async def typing(self, ctx: commands.Context) -> None:
         """Starts typing in every available text channel."""
         coroutines = [channel.trigger_typing() for channel in ctx.guild.text_channels]
         await asyncio.gather(*coroutines)
+
+    @discordo.command()
+    async def live(self, ctx: commands.Context, *, channel: discord.VoiceChannel) -> None:
+        """Anybody want an Opcode 18 payload?"""
+        if not ("discord-user-token" in self.bot.secrets and "discord-user-id" in self.bot.secrets):
+            raise exceptions.SecretNotFound("discord-user secrets not specified in secrets.json.")
+
+        identifier = self.bot.secrets["discord-user-id"]
+        token = self.bot.secrets["discord-user-token"]
+
+        if ctx.guild.get_member(identifier) is None:
+            fail = utilities.Embeds.status(False)
+            fail.description = "Someone is missing..."
+            fail.set_footer(text="Consider inviting them?", icon_url=utilities.Icons.CROSS)
+            return await ctx.reply(embed=fail)
+
+        async with expcord.User(token) as remote:
+            await remote.connect(channel)
+            await remote.stream(channel)
+
+            await asyncio.sleep(5)
+            await remote.disconnect()
 
 def setup(bot: model.Bakerbot) -> None:
     cog = Random(bot)
