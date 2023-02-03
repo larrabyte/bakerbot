5db51240d6c1dac1ccdd950b454d99d0735fbef8
larrabyte <larrabyte@protonmail.ch>
2023-02-03 16:22:56 +1100

    Add some docstrings

--- args.py
+++ args.py
@@ -1,6 +1,7 @@
 import enum
 
 class OptionMeta(enum.EnumMeta):
+    """The backing metaclass behind `Option`."""
     def __iter__(self):
         # Enumeration constants should be kept in uppercase in source,
         # but rendered in lowercase on Discord clients. Since discord.py
@@ -11,4 +12,9 @@ class OptionMeta(enum.EnumMeta):
             yield element
 
 class Option(enum.Enum, metaclass=OptionMeta):
+    """A subclass of `enum.Enum` that renders enumeration names in lowercase."""
     pass
+
+def possibilities(c: OptionMeta) -> str:
+    """Transform an `Option` into a human-readable string of possibilities."""
+    return f"{', '.join((e.name for e in list(c)))}"
