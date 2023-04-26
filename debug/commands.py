e446908131352fddf197e5e287153b431c5cbadb
larrabyte <larrabyte@protonmail.ch>
2023-04-26 21:19:16 +1000

    Require ownership for command synchronisation

--- debug/commands.py
+++ debug/commands.py
@@ -5,7 +5,6 @@ import traceback
 import discord
 import logging
 import colours
-import typing
 import limits
 
 class Debug(commands.GroupCog):
@@ -14,15 +13,17 @@ class Debug(commands.GroupCog):
         self.logger = logger
         self.bot = bot
 
-    @application.command(description="Synchronises application commands.")
-    @application.describe(guild="The guild ID to sync to. If an ID is not specified, a global sync will be executed instead.")
+    @application.command(description="Synchronise application commands.")
+    @application.describe(guild="The guild ID to sync to. If an ID is not specified, a global sync will be executed.")
     @application.describe(overwrite="Whether to copy global commands to the specified guild. This is ignored if a guild ID is not specified.")
     async def sync(self, interaction: discord.Interaction, guild: str | None, overwrite: bool=False) -> None:
-        assert await self.bot.is_owner(interaction.user)
+        if not (await self.bot.is_owner(interaction.user)):
+            await interaction.response.send_message("You are not allowed to execute this command.")
+            return
 
         if guild is None:
             await self.bot.tree.sync()
-            await interaction.response.send_message("Global commands synchronised. Please wait up to **1 hour** for Discord to reflect any changes.", ephemeral=True)
+            await interaction.response.send_message("Global commands synchronised.")
             self.logger.info("Global commands synchronised.")
 
         elif overwrite:
@@ -38,17 +39,17 @@ class Debug(commands.GroupCog):
                 self.logger.info(f"Re-adding command to guild tree: {command}")
                 self.bot.tree.add_command(command, guild=snowflake)
 
-            await interaction.response.send_message(f"Guild commands overwritten.")
+            await interaction.response.send_message("Guild commands overwritten.")
 
         else:
             snowflake = discord.Object(guild)
             await self.bot.tree.sync(guild=snowflake)
-            await interaction.response.send_message(f"Guild-specific commands synchronised.")
-            self.logger.info(f"Guild-specific commands synchronised.")
+            await interaction.response.send_message("Guild-specific commands synchronised.")
+            self.logger.info("Guild-specific commands synchronised.")
 
     async def on_application_error(self, interaction: discord.Interaction, error: application.AppCommandError) -> None:
         trace = "".join(traceback.format_exception(error.__cause__ or error))
-        command = interaction.command.name if interaction.command else "unknown"
+        command = interaction.command.name if interaction.command is not None else "unknown"
 
         self.logger.error(f"Exception raised during execution of '{command}'.")
         self.logger.error(trace)
