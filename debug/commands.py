7d4fb5df64fd088d2cf9d0cb53f59d501df07cdd
larrabyte <larrabyte@protonmail.ch>
2023-05-21 17:16:13 +1000

    Add specific response for permissions failure

--- debug/commands.py
+++ debug/commands.py
@@ -10,34 +10,16 @@ class Debug(commands.GroupCog):
         self.logger = logger
         self.bot = bot
 
-    @application.command(description="Synchronise application commands.")
-    @application.describe(guild="The guild ID to sync to. If an ID is not specified, a global sync will be executed.")
-    @application.describe(overwrite="Whether to copy global commands to the specified guild. This is ignored if a guild ID is not specified.")
-    async def sync(self, interaction: discord.Interaction, guild: str | None, overwrite: bool=False) -> None:
-        if not (await self.bot.is_owner(interaction.user)):
-            await interaction.response.send_message("You are not allowed to execute this command.")
-            return
+    async def interaction_check(self, interaction: discord.Interaction) -> bool:
+        return await self.bot.is_owner(interaction.user)
 
+    @application.command(description="Synchronise application commands.")
+    @application.describe(guild="The guild ID to sync to.")
+    async def sync(self, interaction: discord.Interaction, guild: str | None) -> None:
         if guild is None:
             await self.bot.tree.sync()
             await interaction.response.send_message("Global commands synchronised.")
             self.logger.info("Global commands synchronised.")
-
-        elif overwrite:
-            snowflake = discord.Object(guild)
-            self.bot.tree.clear_commands(guild=snowflake)
-            self.logger.info(f"Command tree cleared for guild ID {guild}.")
-
-            self.bot.tree.copy_global_to(guild=snowflake)
-            await self.bot.tree.sync(guild=snowflake)
-            self.logger.info(f"Global commands copied and synchronised to guild ID {guild}.")
-
-            for command in self.get_app_commands():
-                self.logger.info(f"Re-adding command to guild tree: {command}")
-                self.bot.tree.add_command(command, guild=snowflake)
-
-            await interaction.response.send_message("Guild commands overwritten.")
-
         else:
             snowflake = discord.Object(guild)
             await self.bot.tree.sync(guild=snowflake)
--- debug/errors.py
+++ debug/errors.py
@@ -15,17 +15,25 @@ async def dispatch(interaction: discord.Interaction, **parameters):
 
 async def on_application_error(interaction: discord.Interaction, error: application.AppCommandError):
     """Handles application command errors."""
-    command = interaction.command.name if interaction.command is not None else "unknown"
-    description = "".join(traceback.format_exception(error.__cause__ or error))
+    if isinstance(error, application.CheckFailure):
+        await on_check_failure(interaction, error)
 
-    logger = logging.getLogger(f"bakerbot.{__package__}")
-    logger.error(f"Exception raised during execution of '{command}'.")
-    logger.error(description)
+    else:
+        command = interaction.command.name if interaction.command is not None else "unknown"
+        description = "".join(traceback.format_exception(error.__cause__ or error))
 
-    embed = discord.Embed(
-        title="Exception raised. See below for more information.",
-        description=f"```{limits.limit(description, limits.EMBED_DESCRIPTION - 6)}```",
-        colour=colours.FAILURE
-    )
+        logger = logging.getLogger(f"bakerbot.{__package__}")
+        logger.error(f"Exception raised during execution of '{command}'.")
+        logger.error(description)
 
-    await dispatch(interaction, embed=embed)
+        embed = discord.Embed(
+            title="Exception raised. See below for more information.",
+            description=f"```{limits.limit(description, limits.EMBED_DESCRIPTION - 6)}```",
+            colour=colours.FAILURE
+        )
+
+        await dispatch(interaction, embed=embed)
+
+async def on_check_failure(interaction: discord.Interaction, error: application.CheckFailure):
+    """Handles application command permissions failure."""
+    await dispatch(interaction, content="You are not allowed to execute this command.")
